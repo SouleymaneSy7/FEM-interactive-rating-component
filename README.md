8433c8ef00a2d0415d9f05919872c8ca64225ec0
Souleymane Sy <souleymanesyservices@gmail.com>
2023-07-26 03:02:15 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,71 @@
+# Frontend Mentor - Interactive rating component
+
+![Design preview for the Interactive rating component coding challenge](./design/desktop-preview.jpg)
+
+## Welcome! 👋
+
+This is a solution to the [Interactive rating component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/interactive-rating-component-koxpeBUmI).
+
+[Frontend Mentor](https://www.frontendmentor.io) challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [The challenge](#the-challenge)
+- [Screenshot](#screenshot)
+- [Built with](#built-with)
+- [Links](#links)
+- [Author](#author)
+
+## The challenge
+
+The challenge is to build out this interactive rating component and get it looking as close to the design as possible.
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Select and submit a number rating
+- See the "Thank you" card state after submitting a rating
+
+## Screenshot
+
+This is the Screenshot of my work!
+
+### Mobile
+
+![Mobile Screenshot](./preview/Mobile.png)
+
+### Mobile Success
+
+![Mobile Success Screenshot](./preview/Mobile-Success.png)
+
+### Desktop
+
+![Desktop Screenshot](./preview/Desktop.png)
+
+### Desktop Success
+
+![Desktop Success Screenshot](./preview/Desktop-Success.png)
+
+### Built with
+
+- Semantic HTML5 markup
+- SASS
+- CSS custom properties
+- CSS Grid
+- Mobile-first workflow
+- Vite
+- NPM
+- Vue Js
+- Composition API
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## Author
+
+- GitHub - [Souleymane Sy](https://github.com/SouleymaneSy7)
+- Frontend Mentor - [@SouleymaneSy7](https://www.frontendmentor.io/profile/SouleymaneSy7)
+- Twitter - [@Souleymanesy43](https://twitter.com/Souleymanesy43)
---
+++ src/App.vue
@@ -0,0 +1,140 @@
+<template>
+  <!-- Thank you state start -->
+
+  <section class="success-container" v-if="ratingSuccess">
+    <div class="success__img">
+      <img
+        src="./assets/illustration-thank-you.svg"
+        alt="SVG Thank you Illustration"
+      />
+    </div>
+
+    <div class="selected__text">
+      <p class="rating__paragraph">
+        You selected
+        {{ selectedNumber }}
+        out of 5
+      </p>
+    </div>
+
+    <h2 class="rating__title">Thank you!</h2>
+
+    <p class="rating__paragraph">
+      We appreciate you taking the time to give a rating. If you ever need more
+      support, don’t hesitate to get in touch!
+    </p>
+  </section>
+
+  <!-- Thank you state end -->
+
+  <!-- Rating state start -->
+
+  <main class="rating-container" v-else>
+    <div class="rating__img">
+      <img src="./assets/icon-star.svg" alt="Star Icon" />
+    </div>
+
+    <h1 class="rating__title">How did we do?</h1>
+
+    <p class="rating__paragraph">
+      Please let us know how we did with your support request. All feedback is
+      appreciated to help us improve our offering!
+    </p>
+
+    <ul class="rating__number">
+      <li>
+        <button
+          type="button"
+          class="rating__number--btn"
+          @click="selectNumber(1)"
+        >
+          1
+        </button>
+      </li>
+
+      <li>
+        <button
+          type="button"
+          class="rating__number--btn"
+          @click="selectNumber(2)"
+        >
+          2
+        </button>
+      </li>
+
+      <li>
+        <button
+          type="button"
+          class="rating__number--btn"
+          @click="selectNumber(3)"
+        >
+          3
+        </button>
+      </li>
+
+      <li>
+        <button
+          type="button"
+          class="rating__number--btn"
+          @click="selectNumber(4)"
+        >
+          4
+        </button>
+      </li>
+
+      <li>
+        <button
+          type="button"
+          class="rating__number--btn"
+          @click="selectNumber(5)"
+        >
+          5
+        </button>
+      </li>
+    </ul>
+
+    <button class="rating--btn" type="button" @click="ratingSubmit">
+      Submit
+    </button>
+  </main>
+
+  <!-- Rating state end -->
+
+  <footer class="attribution">
+    <p>
+      Challenge by
+      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
+        >Frontend Mentor</a
+      >
+    </p>
+
+    <p>
+      Coded <span>&hearts;</span> by
+      <a href="https://github.com/SouleymaneSy7" target="_blank"
+        >Souleymane Sy</a
+      >
+    </p>
+  </footer>
+</template>
+
+<script setup>
+// Imports
+import { ref } from "vue";
+
+// Variables
+const selectedNumber = ref(null);
+const ratingSuccess = ref(false);
+
+// Functions
+const selectNumber = (number) => {
+  selectedNumber.value = number;
+};
+
+const ratingSubmit = () => {
+  if (selectedNumber.value === null) {
+    ratingSuccess.value = false;
+  } else {
+    ratingSuccess.value = true;
+  }
+};
+</script>
